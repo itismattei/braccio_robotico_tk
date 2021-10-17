faa98bdf63600f2137b5b1a1b15deb4c0f0f2140
pippo <pippo@pippo.it>
2021-10-17 23:32:15 +0200

    Aggiunta descrizione in main.c

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -6,8 +6,10 @@
   ******************************************************************************
   * @attention
   *
-  * <h2><center>&copy; Copyright (c) 2021 STMicroelectronics.
-  * All rights reserved.</center></h2>
+  * SVILUPPO DEL CODICE PER BRACCIO TINKERCAD.
+  * LINGUAGGIO USATO: C
+  * AMBIENTE STM32IDE
+  * MCU: STM32F722ZE
   *
   * This software component is licensed by ST under BSD 3-Clause license,
   * the "License"; You may not use this file except in compliance with the
